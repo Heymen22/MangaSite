38c45351f64f1665922988a282652254df7d1ce7
Hey_men22 <dpakoh.b.kedax@gmail.com>
2021-11-01 21:14:56 +0300

    remaked index.html layout. Resized last mangas cards.

--- Manga/mainapp/views.py
+++ Manga/mainapp/views.py
@@ -3,5 +3,5 @@ from .models import Manga, MangaChapter
 
 # Create your views here.
 def index(request):
-    chapters = MangaChapter.objects.all()
-    return render(request, 'index.html', context={'chapters': chapters})
+    mangas = Manga.objects.all()
+    return render(request, 'index.html', context={'mangas': mangas})
