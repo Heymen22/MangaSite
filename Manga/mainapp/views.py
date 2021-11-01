6fdde959a7a639938f33a6c2fe5a5b56ab1c6642
Hey_men22 <dpakoh.b.kedax@gmail.com>
2021-11-01 20:16:11 +0300

    remaded index.html

--- Manga/mainapp/views.py
+++ Manga/mainapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .models import Manga
+from .models import Manga, MangaChapter
 
 # Create your views here.
 def index(request):
-    manga = Manga.objects.first()
-    return render(request, 'index.html', context={'manga': manga})
+    chapters = MangaChapter.objects.all()
+    return render(request, 'index.html', context={'chapters': chapters})
