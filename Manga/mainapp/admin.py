f59445111677464015a015e27aa5e63b88909e8c
Hey_men22 <dpakoh.b.kedax@gmail.com>
2021-11-01 16:26:54 +0300

    Reformated files and added models to admin panel

--- Manga/mainapp/admin.py
+++ Manga/mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Manga)
+admin.site.register(MangaChapter)
+admin.site.register(MangaChapterPage)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Tag)
--- Manga/mainapp/models.py
+++ Manga/mainapp/models.py
@@ -12,24 +12,46 @@ class Manga(models.Model):
     raiting = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='Рейтинг манги', default=0,
                                   editable=False)
 
+    def __str__(self):
+        return self.name
+
 
 class MangaChapter(models.Model):
     chapter_number = models.DecimalField(max_digits=4, decimal_places=0, verbose_name='Номер главы')
     author = models.ForeignKey('Author', verbose_name='Автор главы', on_delete=models.SET_NULL, null=True)
     manga = models.ForeignKey('Manga', verbose_name='К какой манге принадлежит глава', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.manga.name} Chapter {self.chapter_number}'
+
+
 class MangaChapterPage(models.Model):
     page_number = models.DecimalField(max_digits=4, decimal_places=0, verbose_name='Номер страницы')
-    chapter = models.ForeignKey('MangaChapter', verbose_name='К какой главе принадлежит страница', on_delete=models.CASCADE)
+    chapter = models.ForeignKey('MangaChapter', verbose_name='К какой главе принадлежит страница',
+                                on_delete=models.CASCADE)
+    page_image = models.ImageField(verbose_name='Страница')
+
+    def __str__(self):
+        return f'{self.chapter.manga.name} Chapter {self.chapter.chapter_number} Page {self.page_number}'
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=30, verbose_name='Имя автора')
     last_name = models.CharField(max_length=30, verbose_name='Фамилия автора')
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название жанра')
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название тэга')
+
+    def __str__(self):
+        return self.name
