554a8b9ade9c43f14f443ac5f5c73972858b3dc2
Hey_men22 <dpakoh.b.kedax@gmail.com>
2021-11-02 18:18:53 +0300

    Remaked index view

--- Manga/mainapp/views.py
+++ Manga/mainapp/views.py
@@ -3,5 +3,5 @@ from .models import Manga, MangaChapter
 
 # Create your views here.
 def index(request):
-    last_5_mangas = Manga.objects.all()
+    last_5_mangas = Manga.objects.all().order_by('-updated_date')[:5]
     return render(request, 'index.html', context={'last_5_mangas': last_5_mangas})
